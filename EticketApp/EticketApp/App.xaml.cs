c549911f773d0eef117041d57c6f50f1e27a01f8
Daniel <dannyb1392@gmail.com>
2022-02-08 20:50:46 -0400

    Add project files.

---
+++ EticketApp/EticketApp/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using EticketApp.ViewModels;
+using EticketApp.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace EticketApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new HomeView();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ EticketApp/EticketApp/ViewModels/HomeViewModel.cs
@@ -0,0 +1,11 @@
+﻿using EticketApp.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EticketApp.ViewModels
+{
+    public class HomeViewModel:ViewModelBase
+    {
+    }
+}
