e18de747714bf783a1f69f8dbfcb68c8a7a41547
Daniel <dannyb1392@gmail.com>
2022-02-08 22:20:36 -0400

    HomeView

---
+++ EticketApp/EticketApp/Models/EticketRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EticketApp.Models
+{
+    public class EticketRequest
+    {
+        public GeneralInformation generalInformation { get; set; }
+        public MigratorialInformation migratorialInformation { get; set;}
+        public CustomsInformation customsInformation { get; set; }
+        public PublicHealthInformation publicHealthInformation { get; set; }
+    }
+}
---
+++ EticketApp/EticketApp/Models/GeneralInformation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EticketApp.Models
+{
+    public class GeneralInformation
+    {
+        public string Cedula { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDay { get; set; }
+        public string AddressResidence { get; set; }
+        public string City { get; set; }
+        public string ContryResidence { get; set; }
+        public string State { get; set; }
+        public bool InOutCountry { get; set; }
+        public string PostalCode { get; set; }
+    }
+}
